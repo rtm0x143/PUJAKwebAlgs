6948d620a31fd2eaa66a41376fef75e391978035
vsuha <s>
2022-03-25 18:50:01 +0700

    main page ulucheniya

---
+++ src/buttonAnimationLaunch.js
@@ -0,0 +1,6 @@
+function expand() {
+    const element = document.getElementById('#nav-circle-button').className = 'nav-button__animation';
+    // element.classList.remove('classname');
+    // void element.offsetWidth;
+    // element.classList.add('classname');
+}
