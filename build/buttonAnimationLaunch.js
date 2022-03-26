11df8cc3b760ca9da3ca689cd372582124824693
vsuha <s>
2022-03-26 12:39:47 +0700

    Main page + menu base logic

--- build/buttonAnimationLaunch.js
+++ build/buttonAnimationLaunch.js
@@ -3,8 +3,8 @@ const overlay = document.getElementById('navigation-screen');
 function openMenu(overlay) {
     const element = document.getElementById('nav-trigger');
     element.addEventListener('click', () => {
-        if (overlay.style.visibility === 'hidden') {
-            overlay.style.visibility = 'visible';
+        if (overlay.className === 'navigation-closed') {
+            overlay.className = 'navigation-opened';
         }
     });
     // element.classList.remove('classname');
@@ -15,8 +15,8 @@ function openMenu(overlay) {
 function closeMenu(overlay) {
     const element = document.getElementById('nav-close');
     element.addEventListener('click', () => {
-        if (overlay.style.visibility === 'visible') {
-            overlay.style.visibility = 'hidden';
+        if (overlay.className === 'navigation-opened') {
+            overlay.className = 'navigation-closed';
         }
     })
 }
