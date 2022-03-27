9b4c2c34128263e175f8a6e66bd564c3e89c1cb1
Andrey Radionov <andrey.radioniv.2003@gmail.com>
2022-03-28 02:04:23 +0700

    bad code structure but in most ways work clasters

--- public/src/View/View.ts
+++ public/src/View/View.ts
@@ -4,66 +4,324 @@ import Errors from '../config/Errors.js';
 import Brush from '../Model/brush.model.js';
 
 class View {
-    body: HTMLBodyElement;
-    regulatorButton: HTMLButtonElement;
-    regulator: HTMLDivElement;
-    regulatorBar: HTMLDivElement;
-    canvasDiv: HTMLDivElement;
-    canvas: HTMLCanvasElement;
-
-    constructor(body: HTMLBodyElement, regulatorButton: HTMLButtonElement, regulator: HTMLDivElement, regulatorBar: HTMLDivElement, canvasDiv: HTMLDivElement, canvas: HTMLCanvasElement, heightParagrath: HTMLParagraphElement) {
-        this.body = body;
-        this.regulatorButton = regulatorButton;
-        this.regulator = regulator;
-        this.regulatorBar = regulatorBar;
-        this.canvasDiv = canvasDiv;
-        this.canvas = canvas;
-
-        this.initSliderListeners(body, regulatorButton, regulator, regulatorBar, canvasDiv, canvas, heightParagrath);
-    }
-
-    initSliderListeners(body: HTMLBodyElement, regulatorButton: HTMLButtonElement, regulator: HTMLDivElement, regulatorBar: HTMLDivElement, canvasDiv: HTMLDivElement, canvas: HTMLCanvasElement, paragraph: HTMLParagraphElement) {
-        let height: number = -regulatorButton.offsetHeight - regulatorBar.offsetHeight;
-        let currentHeight: number;
+    initCanvasSliderListener(
+        size: number,
+        buttonMargin: number,
+        rate: number,
+        body: HTMLBodyElement, 
+        regulatorButton: HTMLButtonElement, 
+        regulator: HTMLDivElement, 
+        regulatorBar: HTMLDivElement,
+        canvasDiv: HTMLDivElement, 
+        canvas: HTMLCanvasElement, 
+        canvasContext: CanvasRenderingContext2D,
+        paragraph: HTMLParagraphElement,
+        selectedОbject: string
+    ) {
+        //2 another params
+        let currentSize: number;
         let isDown: boolean = false;
-        let rate: number = canvasDiv.offsetHeight / -height;
-
+        
         regulatorButton.addEventListener('mousedown', (e) => {
-            currentHeight = e.clientY;
+            currentSize = e.clientY;
             isDown = true;
         })
 
         body.addEventListener('mousemove', (e) => {
             if (isDown) {
-                let change = currentHeight - e.clientY;
-                currentHeight -= change;
-                height -= change
-                
-                if (height > -60) {
-                    height = -60;
+                //try to find change
+                let change = currentSize - e.clientY;
+                currentSize -= change;
+                size += change
+                buttonMargin -= change;
+
+                if (buttonMargin > -regulatorButton.offsetHeight) {
+                    size -= change;
+                    buttonMargin = -regulatorButton.offsetHeight;
                 }
 
-                if (height < -regulator.offsetHeight) {
-                    height = -regulator.offsetHeight;
+                if (buttonMargin < -regulator.offsetHeight) {
+                    size -= change;
+                    buttonMargin = -regulator.offsetHeight;
                 }
 
-                regulatorButton.style.marginTop = `${height}px`;
-                canvasDiv.style.height = ((height * -1) * rate) + 'px';
-                canvas.height = (height * -1) * rate;
-                paragraph.innerHTML = canvas.height.toString();
-                if (height >= -70) {
+                regulatorButton.style.marginTop = `${buttonMargin}px`;
+
+                if (selectedОbject == 'height') {    
+                    canvasDiv.style.height = ((size * -1) * rate) + 'px';
+                    canvas.height = (size * -1) * rate;
+                    paragraph.innerHTML = canvas.height.toString();
+                }
+                else if (selectedОbject == 'width') {
+                    canvasDiv.style.width = ((size * -1) * rate) + 'px';
+                    canvas.width = (size * -1) * rate;
+                    paragraph.innerHTML = canvas.width.toString();
+                }
+                else {
+                    paragraph.innerHTML = Math.floor((size * -1) * rate).toString();
+                }
+
+                if (buttonMargin >= -70) {
                     regulatorBar.style.height = '10px'
                 }
                 else {
-                    regulatorBar.style.height = `${Math.abs(height) - 60}px`
+                    regulatorBar.style.height = `${-buttonMargin - regulatorButton.offsetHeight + 10}px`
                 }
-                
+
+                this.drawGrid(canvasContext, 'grey', Math.floor(canvas.width / 30), Math.floor(canvas.height / 30));
             }
         })
 
         body.addEventListener('mouseup', _ => {
             isDown = false;
         })
+    }
+
+    initDBSCANSliderListener(
+        size: number,
+        buttonMargin: number,
+        rate: number,
+        body: HTMLBodyElement, 
+        regulatorButton: HTMLButtonElement, 
+        regulator: HTMLDivElement, 
+        regulatorBar: HTMLDivElement,
+        canvasDiv: HTMLDivElement, 
+        canvas: HTMLCanvasElement, 
+        canvasContext: CanvasRenderingContext2D,
+        paragraph: HTMLParagraphElement,
+        selectedОbject: string,
+        maxValue: number
+    ) {
+        //2 another params
+        let currentSize: number;
+        let isDown: boolean = false;
+        
+        regulatorButton.addEventListener('mousedown', (e) => {
+            currentSize = e.clientY;
+            isDown = true;
+        })
+
+        body.addEventListener('mousemove', (e) => {
+            if (isDown) {
+                //try to find change
+                let change = currentSize - e.clientY;
+                currentSize -= change;
+                size += change
+                buttonMargin -= change;
+
+                if (buttonMargin > -regulatorButton.offsetHeight) {
+                    size = 0;
+                    buttonMargin = -regulatorButton.offsetHeight;
+                }
+
+                if (buttonMargin < -regulator.offsetHeight) {
+                    size = maxValue / rate;
+                    buttonMargin = -regulator.offsetHeight;
+                }
+
+                regulatorButton.style.marginTop = `${buttonMargin}px`;
+                regulatorBar.style.height = `${-buttonMargin - 55}px`;
+
+                paragraph.innerHTML = Math.floor(size * rate).toString();
+
+                if (buttonMargin >= -70) {
+                    regulatorBar.style.height = `${10}px`;
+                }
+                
+
+                // if (buttonMargin < -regulator.offsetHeight) {
+                //     size -= change;
+                //     buttonMargin = -regulator.offsetHeight;
+                // }
+            }
+        })
+
+        body.addEventListener('mouseup', _ => {
+            isDown = false;
+        })
+    }
+
+
+    // initSliderListeners(
+    //     body: HTMLBodyElement, 
+    //     regulatorButton: HTMLButtonElement, 
+    //     regulator: HTMLDivElement, 
+    //     canvasDiv: HTMLDivElement, 
+    //     canvas: HTMLCanvasElement,
+    // ) {
+    //     //the real time size in slider
+    //     let size: number = 0;
+    //     let buttonMargin: number = 10;
+    //     if (regulatorButton.className=="regulator__button regulator__button_height") {
+    //         size = regulatorButton.offsetHeight + this.heightRegulatorBar.offsetHeight;
+    //         buttonMargin = -regulatorButton.offsetHeight - this.heightRegulatorBar.offsetHeight;
+    //     }
+    //     else if (regulatorButton.className=="regulator__button regulator__button_width") {
+    //         size = this.widthRegulatorBar.offsetHeight;
+    //         buttonMargin = -regulatorButton.offsetHeight - this.widthRegulatorBar.offsetHeight;
+    //     }
+    //     else if (regulatorButton.className=="regulator__button regulator__button_range") {
+    //         size = this.rangeRegulatorBar.offsetHeight;
+    //         buttonMargin = -regulatorButton.offsetHeight - this.rangeRegulatorBar.offsetHeight;
+    //     }
+    //     else {
+    //         size = this.countRegulatorBar.offsetHeight;
+    //         buttonMargin = -regulatorButton.offsetHeight - this.rangeRegulatorBar.offsetHeight;
+    //     }
+
+    //     //2 another params
+    //     let currentSize: number;
+    //     let isDown: boolean = false;
+        
+    //     //k for changing size
+    //     let rate: number;
+    //     if (regulatorButton.className == "regulator__button regulator__button_height") {
+    //         rate = canvasDiv.offsetHeight / -size;
+    //     } 
+    //     else if (regulatorButton.className=="regulator__button regulator__button_width") {
+    //         rate = canvasDiv.offsetWidth / -size;
+    //     }
+    //     else if (regulatorButton.className=="regulator__button regulator__button_range") {
+    //         rate = canvasDiv.offsetWidth / -size;
+    //     }
+    //     else {
+    //         console.log(this.heightRegulator.offsetHeight);
+    //         rate = 1 / ((this.heightRegulator.offsetHeight - 60) / 255);
+    //     }
+
+    //     regulatorButton.addEventListener('mousedown', (e) => {
+    //         currentSize = e.clientY;
+    //         isDown = true;
+    //     })
+
+    //     body.addEventListener('mousemove', (e) => {
+    //         if (isDown) {
+    //             //try to find change
+    //             let change = currentSize - e.clientY;
+    //             currentSize -= change;
+    //             size += change
+    //             buttonMargin -= change;
+
+    //             if (regulatorButton.className == "regulator__button regulator__button_height") {
+    //                 if (buttonMargin > -regulatorButton.offsetHeight) {
+    //                     size -= change;
+    //                     buttonMargin = -regulatorButton.offsetHeight;
+    //                 }
+    
+    //                 if (buttonMargin < -regulator.offsetHeight) {
+    //                     size -= change;
+    //                     buttonMargin = -regulator.offsetHeight;
+    //                 }
+
+    //                 regulatorButton.style.marginTop = `${buttonMargin}px`;
+                    
+    //                 canvasDiv.style.height = ((size * -1) * rate) + 'px';
+    //                 canvas.height = (size * -1) * rate;
+    //                 this.heightParagrapth.innerHTML = canvas.height.toString();
+
+    //                 if (buttonMargin >= -70) {
+    //                     this.heightRegulatorBar.style.height = '10px'
+    //                 }
+
+    //                 else {
+    //                     this.heightRegulatorBar.style.height = `${Math.abs(size) - regulatorButton.offsetHeight}px`
+    //                 }
+
+    //             } 
+    //             else if (regulatorButton.className == "regulator__button regulator__button_width") {
+    //                 if (buttonMargin > -regulatorButton.offsetHeight) {
+    //                     size -= change;
+    //                     buttonMargin = -regulatorButton.offsetHeight;
+    //                 }
+    
+    //                 if (buttonMargin < -regulator.offsetHeight) {
+    //                     size -= change;
+    //                     buttonMargin = -regulator.offsetHeight;
+    //                 }
+
+    //                 regulatorButton.style.marginTop = `${buttonMargin}px`;
+
+    //                 canvasDiv.style.width = ((size * -1) * rate) + 'px';
+    //                 canvas.width = (size * -1) * rate;
+    //                 this.canvasMenu.style.width = (size * -1) * rate + 'px';
+    //                 this.widthParagrapth.innerHTML = canvas.width.toString();
+
+    //                 if (buttonMargin >= -70) {
+    //                     this.widthRegulatorBar.style.height = '10px'
+    //                 }
+    //                 else {
+    //                     this.widthRegulatorBar.style.height = `${size}px`
+    //                 }
+
+    //             }
+    //             else if (regulatorButton.className == "regulator__button regulator__button_range") {
+    //                 if (buttonMargin > -regulatorButton.offsetHeight) {
+    //                     buttonMargin = -regulatorButton.offsetHeight;
+    //                 }
+    
+    //                 if (buttonMargin < -regulator.offsetHeight) {
+    //                     buttonMargin = -regulator.offsetHeight;
+    //                 }
+
+    //                 regulatorButton.style.marginTop = `${buttonMargin}px`;
+
+    //                 if (size > this.heightRegulator.offsetHeight - 60) {
+    //                     size -= change;
+    //                 }
+    
+    //                 if (size < 0) {
+    //                     size = 0;
+    //                 }
+
+    //                 this.rangeParagrapth.innerHTML = size.toString();
+    //                 this.rangeRegulatorBar.style.height = `${size}px`;
+    //                 if (size <= 10) {
+    //                     this.rangeRegulatorBar.style.height = `${10}px`;
+    //                 }
+    //             }
+    //             else {
+    //                 if (buttonMargin > -regulatorButton.offsetHeight) {
+    //                     buttonMargin = -regulatorButton.offsetHeight;
+    //                 }
+    
+    //                 if (buttonMargin < -regulator.offsetHeight) {
+    //                     buttonMargin = -regulator.offsetHeight;
+    //                 }
+
+    //                 regulatorButton.style.marginTop = `${buttonMargin}px`;
+
+    //                 if (size * rate > 255) {
+    //                     size = 255 / rate;
+    //                 }
+    
+    //                 if (size * rate < 0) {
+    //                     size = 0;
+    //                 }
+                    
+    //                 this.countParagrapth.innerHTML = Math.floor(size * rate).toString();
+    //                 this.countRegulatorBar.style.height = `${size}px`;
+
+    //                 if (size * rate <= 10) {
+    //                     this.countRegulatorBar.style.height = `${10}px`;
+    //                 }
+    //             }
+
+    //             this.drawGrid(this.canvasContext, 'grey', Math.floor(canvas.width / 30), Math.floor(canvas.height / 30));                
+    //         }
+    //     })
+
+    //     body.addEventListener('mouseup', _ => {
+    //         isDown = false;
+    //     })
+    // }
+
+    drawGrid(context: CanvasRenderingContext2D, color: string, columnCount: number, rowsCount: number) {
+        context.strokeStyle = color;
+        for (let i = 0; i < columnCount; ++i) {
+            for (let j = 0; j < rowsCount; ++j) {
+                context.strokeRect(i * this.canvas.width / columnCount, j * this.canvas.height / rowsCount, this.canvas.width / columnCount, this.canvas.height / rowsCount);
+            }
+        }
     }
 
     drawCircle(canvasContext: CanvasRenderingContext2D, strokeColor: string, fillColor: string, x: number, y: number, radius: number) {
--- public/src/View/canvas.claster.view.ts
+++ public/src/View/canvas.claster.view.ts
@@ -4,9 +4,6 @@ import ClasterModel from '../Model/claster.model.js';
 import View from './View.js';
 
 class CanvasClasterView extends View {
-    // private _body: HTMLBodyElement;
-    private _canvasContext: CanvasRenderingContext2D;
-
     private _regulatorButton: HTMLButtonElement;
     // private _regulator: HTMLDivElement;
 
@@ -19,17 +16,42 @@ class CanvasClasterView extends View {
     private _clasterModel: ClasterModel;
     //private _brush: Brush;
 
-    constructor(model: ClasterModel) {
-        super(document.querySelector('body') ?? Errors.handleError('null'),
-                document.querySelector('.regulator__button') ?? Errors.handleError('null'),
-                document.querySelector('.regulator__width-param') ?? Errors.handleError('null'),
-                document.querySelector('.regulator__bar') ?? Errors.handleError('null'),
-                document.querySelector('.canvas') ?? Errors.handleError('null'),
-                document.querySelector('.canvas__element') ?? Errors.handleError('null'),
-                document.querySelector('.regulator__height-paragraph') ?? Errors.handleError('null'));
+    private body: HTMLBodyElement;
+    private regulatorButtons: NodeListOf<HTMLButtonElement>;
+    private widthRegulator: HTMLDivElement;
+    private heightRegulator: HTMLDivElement;
+    private heightRegulatorBar: HTMLDivElement;
+    private widthRegulatorBar: HTMLDivElement;
+    private rangeRegulatorBar: HTMLDivElement;
+    private countRegulatorBar: HTMLDivElement;
+    private canvasDiv: HTMLDivElement;
+    private canvas: HTMLCanvasElement;
+    private canvasContext: CanvasRenderingContext2D;
+    private heightParagrapth: HTMLParagraphElement;
+    private widthParagrapth: HTMLParagraphElement;
+    private rangeParagrapth: HTMLParagraphElement;
+    private countParagrapth: HTMLParagraphElement;
+    private canvasMenu: HTMLDivElement;
 
-        //Canvas element and context
-        this._canvasContext = this.canvas.getContext('2d') ?? Errors.handleError('null');
+    constructor(model: ClasterModel) {
+        super();
+
+        this.body = document.querySelector('body') ?? Errors.handleError('null');
+        this.regulatorButtons = document.querySelectorAll('.regulator__button') ?? Errors.handleError('null');
+        this.heightRegulator = document.querySelector('.regulator__width-param') ?? Errors.handleError('null');
+        this.widthRegulator = document.querySelector('.regulator__height-param') ?? Errors.handleError('null');
+        this.heightRegulatorBar = document.querySelector('.regulator__bar_height') ?? Errors.handleError('null');
+        this.widthRegulatorBar = document.querySelector('.regulator__bar_width') ?? Errors.handleError('null');
+        this.rangeRegulatorBar = document.querySelector('.regulator__bar_range') ?? Errors.handleError('null');
+        this.countRegulatorBar = document.querySelector('.regulator__bar_count') ?? Errors.handleError('null');
+        this.canvasDiv = document.querySelector('.canvas') ?? Errors.handleError('null');
+        this.canvas = document.querySelector('.canvas__element') ?? Errors.handleError('null');
+        this.canvasContext = this.canvas.getContext('2d') ?? Errors.handleError('null');
+        this.heightParagrapth = document.querySelector('.regulator__height-paragraph') ?? Errors.handleError('null');
+        this.widthParagrapth = document.querySelector('.regulator__width-paragraph') ?? Errors.handleError('null');
+        this.rangeParagrapth = document.querySelector('.regulator__range-paragraph') ?? Errors.handleError('null');
+        this.countParagrapth = document.querySelector('.regulator__count-paragraph') ?? Errors.handleError('null');
+        this.canvasMenu = document.querySelector('.canvas__menu') ?? Errors.handleError('null');
 
         //Elements fo change canvas
         this._regulatorButton = document.querySelector('.regulator__button') ?? Errors.handleError('null');
@@ -48,6 +70,76 @@ class CanvasClasterView extends View {
         this._subscribe();
         this.canvas.setAttribute('height', this.canvasDiv.offsetHeight.toString());
         this.canvas.setAttribute('width', this.canvasDiv.offsetWidth.toString());
+        this.regulatorButtons.forEach((item) => {
+            if (item.className == "regulator__button regulator__button_height") {
+                this.initCanvasSliderListener(
+                    item.offsetHeight + this.heightRegulatorBar.offsetHeight,
+                    -item.offsetHeight - this.heightRegulatorBar.offsetHeight,
+                    this.canvasDiv.offsetHeight / -(item.offsetHeight + this.heightRegulatorBar.offsetHeight),
+                    this.body,
+                    item,
+                    this.heightRegulator,
+                    this.heightRegulatorBar,
+                    this.canvasDiv,
+                    this.canvas,
+                    this.canvasContext,
+                    this.heightParagrapth,
+                    'height'
+                )
+            }
+            else if (item.className == "regulator__button regulator__button_width") {
+                this.initCanvasSliderListener(
+                    item.offsetHeight + this.widthRegulatorBar.offsetHeight,
+                    -item.offsetHeight - this.widthRegulatorBar.offsetHeight,
+                    this.canvasDiv.offsetWidth / -(item.offsetHeight + this.widthRegulatorBar.offsetHeight),
+                    this.body,
+                    item,
+                    this.widthRegulator,
+                    this.widthRegulatorBar,
+                    this.canvasDiv,
+                    this.canvas,
+                    this.canvasContext,
+                    this.widthParagrapth,
+                    'width'
+                )
+            }
+            else if (item.className == "regulator__button regulator__button_range") {
+                this.initDBSCANSliderListener(
+                    this.rangeRegulatorBar.offsetHeight,
+                    -item.offsetHeight - this.rangeRegulatorBar.offsetHeight,
+                    1.5,
+                    this.body,
+                    item,
+                    this.widthRegulator,
+                    this.rangeRegulatorBar,
+                    this.canvasDiv,
+                    this.canvas,
+                    this.canvasContext,
+                    this.rangeParagrapth,
+                    'range',
+                    960
+                )
+            }
+            else if (item.className == "regulator__button regulator__button_count") {
+                this.initDBSCANSliderListener(
+                    this.countRegulatorBar.offsetHeight,
+                    -item.offsetHeight - this.rangeRegulatorBar.offsetHeight,
+                    1 / ((this.heightRegulator.offsetHeight - 60) / 255),
+                    this.body,
+                    item,
+                    this.widthRegulator,
+                    this.countRegulatorBar,
+                    this.canvasDiv,
+                    this.canvas,
+                    this.canvasContext,
+                    this.countParagrapth,
+                    'count',
+                    255
+                )
+            }
+        })
+        
+        this.drawGrid(this.canvasContext, 'grey', Math.floor(this.canvas.width / 30), Math.floor(this.canvas.height / 30));
     }
 
     getMousePosition(event: MouseEvent): Object {
@@ -67,7 +159,7 @@ class CanvasClasterView extends View {
     _subscribe() {
         this._clasterModel.addEventListener('claster.model:addObj', () => {
             this.drawCircle(
-                this._canvasContext,
+                this.canvasContext,
                 '',
                 'red', 
                 this._clasterModel.positions[this._clasterModel.positions.length - 1],
@@ -79,7 +171,7 @@ class CanvasClasterView extends View {
     handleDBSCANFetch(callback: Function) {
         this._DBSCANButton.addEventListener('click', (event) => {
             event.preventDefault();
-            callback(this._canvasContext, parseInt("30"), parseInt("3"));
+            callback(this.canvasContext, parseInt("30"), parseInt("3"));
         })
     }    
 
