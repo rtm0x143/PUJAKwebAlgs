51c1590622b512f4a589519b33468971d97f3d76
Andrey Radionov <andrey.radioniv.2003@gmail.com>
2022-03-31 06:33:59 +0700

    js changes

--- public/dist/Controller/Controller.js
+++ public/dist/Controller/Controller.js
@@ -1,18 +1,39 @@
 class Controller {
-    constructor(model, newText) {
-        this.model = model;
-        this.newText = newText;
+    constructor() {
+        this.urlValue = location.origin;
     }
-    changeTextHandler(newText) {
-        this.model.changeTextValue(newText.value);
-    }
-    handleEvent(e) {
-        e.stopPropagation();
-        switch (e.target.className) {
-            case "text_change":
-                this.changeTextHandler(this.newText);
-                break;
+    hsvToRGB(h, s, v) {
+        /// Parametres for convert 
+        /// C = V * S
+        /// X = C * |(1 - H / 60 % 2 - 1|)
+        /// m = V - C
+        /// R'G'B'
+        /// (C, X, 0)  <--- 0 degr   <= H < 60 degr
+        /// (X, C, 0)  <--- 60 degr  <= H < 120 degr
+        /// (0, C, X)  <--- 120 degr <= H < 180 degr
+        /// (0, X, C)  <--- 180 degr <= H < 240 degr
+        /// (X, 0, C)  <--- 240 degr <= H < 300 degr
+        /// (C, 0, X)  <--- 300 degr <= H < 360 degr
+        ///R'G'B' ---> (R, G, B) === ((R'+m)*255, (G'+m)*255, (B'+m)*255)
+        if (h < 60) {
+            return `rgb(${(v * s + v - v * s) * 255},${(v * s * Math.abs(1 - (h / 60) % 2 - 1) + v - v * s) * 255},0)`;
         }
+        else if (h < 120) {
+            return `rgb(${(v * s * Math.abs(1 - (h / 60) % 2 - 1) + v - v * s) * 255},${(v * s + v - v * s) * 255},0)`;
+        }
+        else if (h < 180) {
+            return `rgb(0,${(v * s + v - v * s) * 255},${(v * s * Math.abs(1 - (h / 60) % 2 - 1) + v - v * s) * 255})`;
+        }
+        else if (h < 240) {
+            return `rgb(0,${(v * s * Math.abs(1 - (h / 60) % 2 - 1) + v - v * s) * 255},${(v * s + v - v * s) * 255})`;
+        }
+        else if (h < 300) {
+            return `rgb(${(v * s * Math.abs(1 - (h / 60) % 2 - 1) + v - v * s) * 255},0, ${(v * s + v - v * s) * 255})`;
+        }
+        else if (h < 360) {
+            return `rgb(${(v * s + v - v * s) * 255},0,${(v * s * Math.abs(1 - (h / 60) % 2 - 1) + v - v * s) * 255})`;
+        }
+        return '';
     }
 }
 export default Controller;
