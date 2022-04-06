752b030615ae5a842020069bd6a8332983a5ebeb
andronzi <andrey.radioniv.2003@gmail.com>
2022-04-06 02:21:37 -0700

    finish clasters without fix on back and fixes with template

--- public/src/Controller/canvas.claster.controller.ts
+++ public/src/Controller/canvas.claster.controller.ts
@@ -5,7 +5,7 @@ import Controller from "./Controller.js";
 
 class ClasterController extends Controller{
     private _clasterView: ClasterView;
-    private _clasterModel: ClasterModel;
+    private readonly _clasterModel: ClasterModel;
 
     constructor(clasterModel: ClasterModel) {
         super();
@@ -48,9 +48,9 @@ class ClasterController extends Controller{
                 body: (new Uint16Array(this._clasterModel.positions)).buffer
             }).then((response) => {
                 let reader = response.body?.getReader();
-                reader?.read().then(({done, value}) => {
-                    if (value == undefined) Errors.handleError('undefined');
-                    if (value == null) Errors.handleError('null');
+                reader?.read().then(({value}) => {
+                    if (value === undefined) Errors.handleError('undefined');
+                    if (value === null) Errors.handleError('null');
                     let colorsArray = []
 
                     for (let i = 1; i < value.length; i += 2) {
@@ -98,7 +98,9 @@ class ClasterController extends Controller{
                 })
             })
         } else {
-            fetch(`${this.urlValue}/alg/clasterisation?type=k_means&pCount=${2}&metric=${metricType}[&cCount=${clastersCount}]`, {
+            fetch(clastersCount ?
+            `${this.urlValue}/alg/clasterisation?type=k_means&pCount=${2}&metric=${metricType}&cCount=${clastersCount}`:
+                `${this.urlValue}/alg/clasterisation?type=k_means&pCount=${2}&metric=${metricType}`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/octet-stream'
@@ -107,53 +109,45 @@ class ClasterController extends Controller{
                 body: (new Uint16Array(this._clasterModel.positions)).buffer
             }).then((response) => {
                 let reader = response.body?.getReader();
-                reader?.read().then(({done, value}) => {
-                    if (value == undefined) Errors.handleError('undefined');
-                    if (value == null) Errors.handleError('null');
+                reader?.read().then(({ value}) => {
+                    console.log(value);
+                    if (value === undefined) Errors.handleError('undefined');
+                    if (value === null) Errors.handleError('null');
                     let colorsArray = []
 
-                    for (let i = 1; i < value.length; i += 2) {
-                        if (value[i] >= 2) {
-                            if (value[i - 1] > colorsArray.length) {
-                                for (let j = colorsArray.length; j < value[i - 1]; ++j) {
-                                    colorsArray.push(this.hsvToRGB(
-                                        Math.floor(
-                                            Math.random() * 361
-                                        ),
-                                        1,
-                                        0.8 + Math.random() * 0.2) ?? Errors.handleError('undefined')
-                                    );
-                                }
-
-                                this._clasterView.drawCircle(
-                                    context,
-                                    '',
-                                    colorsArray[value[i - 1] - 1],
-                                    this._clasterModel.positions[i],
-                                    this._clasterModel.positions[i - 1],
-                                    radius
-                                );
-                            } else {
-                                this._clasterView.drawCircle(
-                                    context,
-                                    '',
-                                    colorsArray[value[i - 1] - 1],
-                                    this._clasterModel.positions[i],
-                                    this._clasterModel.positions[i - 1],
-                                    radius
+                    for (let i = 0, k = 0; i < value.length; ++i, k += 2) {
+                        if (value[i] + 1 > colorsArray.length) {
+                            for (let j = colorsArray.length; j < value[i] + 1; ++j) {
+                                colorsArray.push(this.hsvToRGB(
+                                    Math.floor(
+                                        Math.random() * 361
+                                    ),
+                                    1,
+                                    1) ?? Errors.handleError('undefined')
                                 );
                             }
+
+                            this._clasterView.drawCircle(
+                                context,
+                                '',
+                                colorsArray[value[i]],
+                                this._clasterModel.positions[k + 1],
+                                this._clasterModel.positions[k],
+                                radius
+                            );
                         } else {
                             this._clasterView.drawCircle(
                                 context,
                                 '',
-                                'grey',
-                                this._clasterModel.positions[i],
-                                this._clasterModel.positions[i - 1],
+                                colorsArray[value[i]],
+                                this._clasterModel.positions[k + 1],
+                                this._clasterModel.positions[k],
                                 radius
                             );
                         }
                     }
+
+                    console.log(colorsArray);
                 })
             })
         }
